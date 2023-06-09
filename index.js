d9eef07bb2be950556797a14c4eab1c3b7b8bf51
monsur776e <monsuralam8897@gmail.com>
2023-06-10 02:42:52 +0600

    admin and instructor api word sucessfully

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.get("/", (req, res) => {
    res.send("shutter school is runnig ");
 });
 
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.kjf5ogd.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -59,6 +59,34 @@ async function run() {
       // find user collection
       app.get("/users", async (req, res) => {
          const result = await userCollection.find().toArray();
+         res.send(result);
+      });
+
+      //   make admin account
+      app.patch("/users/admin/:id", async (req, res) => {
+         const id = req.params.id;
+         const filter = { _id: new ObjectId(id) };
+         const updateDoc = {
+            $set: {
+               role: "admin",
+            },
+         };
+
+         const result = await userCollection.updateOne(filter, updateDoc);
+         res.send(result);
+      });
+
+      //   make instructor account
+      app.patch("/users/instructor/:id", async (req, res) => {
+         const id = req.params.id;
+         const filter = { _id: new ObjectId(id) };
+         const updateDoc = {
+            $set: {
+               role: "instructor",
+            },
+         };
+
+         const result = await userCollection.updateOne(filter, updateDoc);
          res.send(result);
       });
 
